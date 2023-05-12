c9dbd1f4b049712cba2f57462c6255990a02fcbf
vburenkov <vburenkov@mail.ru>
2023-05-12 20:59:30 +0300

    Update Program.cs

--- Parsers/FilmParser/FilmParser/Program.cs
+++ Parsers/FilmParser/FilmParser/Program.cs
@@ -1,10 +1,11 @@
 ﻿using FilmParser;
 
 IFilmCommentsParser parser = new MetacriticFilmParser();
-Film f = parser.GetFilmByName("Rambo");
+Film f = parser.GetFilmByName("Home Alone");
 var comments = parser.GetFilmComments(f);
 foreach (var comment in comments)
 {
+
     Console.WriteLine(comment);
     Console.WriteLine("=========================");
 }
